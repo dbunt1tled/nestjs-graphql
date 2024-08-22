64fab58396a5f996faf7bfc8aa3e3e487ca10394
Denis <unt1tled@ua.fm>
2024-08-22 11:39:15 +0300

    feat(user): files

--- src/core/config-api/file.config.ts
+++ src/core/config-api/file.config.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PathInfo } from 'src/core/config-api/dto/path-info.dto';
-import path from 'path';
 
 @Injectable()
 export class FileConfig {
